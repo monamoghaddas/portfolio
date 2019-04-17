94507a57c46aec48364bf5095711e3771ed789c9
mona moghaddas <moghaddas.mona@gmail.com>
2019-04-17 00:04:25 -0700

    fullname animation added

--- src/App.js
+++ src/App.js
@@ -8,8 +8,12 @@ class App extends Component {
       <div className="Portfolio">
         <header className="pf-header overlay">
           <div className="container">
-            <h1 className="myname animated growIn slower go">Mona Moghaddas</h1>
-          </div>
+            <div class="animate fullname">
+			           <span>M</span><span>O</span><span>N</span><span>A</span> &nbsp;
+			           <span>M</span><span>O</span><span>G</span><span>H</span><span>A</span><span>D</span>
+                 <span>D</span><span>A</span><span>S</span>
+            </div>
+        </div>
         </header>
         <div className="main">
           <p>test</p>
