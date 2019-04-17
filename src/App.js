d7ed2f51692380bd29b67102fd1fd4f97a53cc9a
mona moghaddas <moghaddas.mona@gmail.com>
2019-04-16 23:28:23 -0700

    no message

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './assets/styles/App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="Portfolio">
+        <header className="pf-header overlay">
+          <div className="container">
+            <h1 className="myname animated growIn slower go">Mona Moghaddas</h1>
+          </div>
+        </header>
+        <div className="main">
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
