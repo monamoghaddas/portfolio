8b6de41b09c1b4d4676f55a365fef990dacb2e35
mona moghaddas <moghaddas.mona@gmail.com>
2019-05-01 20:21:58 -0700

    edit testimonial

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import Header from './components/header/header';
 import About from './components/about/about';
 import Resume from './components/resume/resume';
 import Portfolio from './components/portfolio/portfolio';
-import Testimonials from  './components/testimonials/testimonials';
 import Footer from './components/footer/footer';
+{/*import Testimonials from  './components/testimonials/testimonials';*/}
 {/*import ContactUs from './components/contactus/contactus';*/}
 
 class App extends Component {
@@ -15,7 +15,7 @@ class App extends Component {
         <About />
         <Resume />
         <Portfolio />
-        <Testimonials />
+        {/*}<Testimonials />*/}
         {/*<ContactUs />*/}
         <Footer />
       </div>
